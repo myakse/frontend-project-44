1f6a8fb70535492162d0958e5061f136fc281e69
Ksunya <Ksenia.Yakimenko.6@yandex.ru>
2022-11-03 23:58:18 +0400

    scr/even.js

--- scr/even.js
+++ scr/even.js
@@ -1,23 +1,23 @@
 import readlineSync from 'readline-sync';
 
 const evenGames = () => {
-  console.log('Welcome to the Brain Games!');  
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name?');
   console.log(`Hello, ${userName}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".')
-    
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
   for (let i = 0; i < 3; i += 1) {
-  const randomNumber = Math.round(Math.random() * 100);
-  console.log(`Question: ${randomNumber}`);
-  const userAnswer = readlineSync.question('Your answer: ');
-  const probabledAnswer = userAnswer === 'yes' ? "no" : "yes";
-  if ((randomNumber % 2 === 0 && userAnswer === 'yes') || (randomNumber % 2 !== 0 && userAnswer === 'no')) {
-    console.log('Correct!');
-  } else {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${probabledAnswer}'.`)
-    console.log(`Let's try again, ${userName}!`);
-    return;
-  };  
+    const randomNumber = Math.round(Math.random() * 100);
+    console.log(`Question: ${randomNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const probabledAnswer = userAnswer === 'yes' ? 'no' : 'yes';
+    if ((randomNumber % 2 === 0 && userAnswer === 'yes') || (randomNumber % 2 !== 0 && userAnswer === 'no')) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${probabledAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
   }
   console.log(`Congratulations, ${userName}!`);
 };
